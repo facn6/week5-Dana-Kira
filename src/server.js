b5948f61d3e5f2fb82e1d5e2c88613c15d80d2fd
DanaEFalah <dana.e.falah@gmail.com>
2019-07-31 18:19:11 +0300

    setup readme file

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const router = require('./routers');
 
 const hostname = process.env.HOSTNAME || 'localhost';
-const port = process.env.PORT || 4005;
+const port = process.env.PORT ||process.env.port || 4005;
 
 http.createServer(router).listen(port, hostname, () => {
   console.log(`the server is running on port http://${hostname}:${port}`);
