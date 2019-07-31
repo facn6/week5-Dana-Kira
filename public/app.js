22a9a8245e323914c635ebb165843c2807b116eb
DanaEFalah <dana.e.falah@gmail.com>
2019-07-31 17:16:14 +0300

    update front end files

--- public/app.js
+++ public/app.js
@@ -2,30 +2,30 @@
 function getSelectValue()
 {
     var selectedValue = document.getElementById("list").value;
-    console.log(selectedValue);
     fetch(`/${selectedValue}`).then((res)=>{
         return res.json();
     }).then((data)=>{
         var result = document.getElementById('result1');
-
-        for(let i=0;i<data.length;i++){
-            var card=document.createElement('div');
-            card.setAttribute('class','card');
-            card.setAttribute('id',i);
-           
-            
-            var label = document.createElement('label'); 
-            label.innerHTML = data[i].webTitle; 
-            card.appendChild(label);
-            var link = document.createElement('a'); 
-            link.href = data[i].webUrl;
-            link.innerHTML ='Read The Article Online';
-            console.log("link" , link);
-            card.appendChild(link); 
-
-            result.appendChild(card);
+        while (result.hasChildNodes()) {
+            result.removeChild(result.firstChild);
         } 
-    });
+        for(let i=0;i<data.length;i++){
+        var card=document.createElement('div');
+        card.setAttribute('class','card');
+        card.setAttribute('id',i);
+        
+        
+        var label = document.createElement('label'); 
+        label.innerHTML = data[i].webTitle; 
+        card.appendChild(label);
+        var link = document.createElement('a'); 
+        link.href = data[i].webUrl;
+        link.innerHTML ='Read The Article Online';
+        card.appendChild(link); 
+
+        result.appendChild(card);
+    } 
+});
 }
 
 
